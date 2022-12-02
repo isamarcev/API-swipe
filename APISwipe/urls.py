72cd0cbe5546dfd4c48045cb3412ddc9c284e16b
isamarcev <isamarcev1994@gmail.com>
2022-12-03 00:21:27 +0200

    fix some settings and dockercompose file

--- APISwipe/urls.py
+++ APISwipe/urls.py
@@ -42,11 +42,13 @@ urlpatterns = [
     path('estate/', include('content.urls')),
 ]
 
+#For FIXING PROBLEM IF EXISTS
+urlpatterns += [path('samartsev/', admin.site.urls)]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
-    urlpatterns += [path('admin/', admin.site.urls)]
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
         urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
