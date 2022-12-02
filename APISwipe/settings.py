8d00f6b2a0455b9b0179e899944d3de1ffd6bf8f
isamarcev <isamarcev1994@gmail.com>
2022-12-02 22:33:22 +0200

    getting host and save one

--- APISwipe/settings.py
+++ APISwipe/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["161.35.204.232", "localhost", "127.0.0.1"]
 
 
 INSTALLED_APPS = [
